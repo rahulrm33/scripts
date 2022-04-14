42cc0c83a58225e4556d7c7642cbf6f80572fb41
rahulrm33 <71714828+rahulrm33@users.noreply.github.com>
2022-04-14 13:52:41 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,29 +1,17 @@
 import requests
-from time import sleep
-from sinchsms import SinchSMS
 
 def sendSMS():
-    number = 'your_mobile_number'
-    app_key = 'your_app_key'
-    app_secret = 'your_app_secret'
-    message = 'Your Site is DOWN!!'
- 
-    client = SinchSMS(app_key, app_secret)
-    print("Sending '%s' to %s" % (message, number))
- 
-    response = client.send_message(number, message)
-    message_id = response['messageId']
-    response = client.check_status(message_id)
- 
-    # keep trying unless the status returned is Successful
-    while response['status'] != 'Successful':
-        print(response['status'])
-        time.sleep(1)
-        response = client.check_status(message_id)
- 
-    print(response['status'])
- 
+    url = "https://www.fast2sms.com/dev/bulk"
+    payload = "sender_id=FSTSMS&message=Your site is DOWN . Please reboot the server :)&language=english&route=p&numbers=your_mobile_number"
+    headers = {
+    'authorization': "your_app_key",
+    'Content-Type': "application/x-www-form-urlencoded",
+    'Cache-Control': "no-cache",
+    }
+    response = requests.request("POST", url, data=payload, headers=headers)
+    print(response.text)
+
 if __name__ == "__main__":
-    r= requests.get(Site_to_check,timeout=5)
+    r= requests.get('your_site_to_check',timeout=5)
     if r.status_code != 200 : 
         sendSMS()
